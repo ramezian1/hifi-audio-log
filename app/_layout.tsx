6374be2a7904bad0da4797c3cf10d2e4aadce1b6
Robert Mezian <163072227+ramezian1@users.noreply.github.com>
2026-04-10 02:08:52 -0700

    Wrap SafeAreaProvider with GestureHandlerRootView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from 'expo-router';
 import { MD3DarkTheme, PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SafeAreaProvider>
       <PaperProvider theme={MD3DarkTheme}>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(tabs)" />
@@ -51,5 +53,6 @@ export default function RootLayout() {
         </Stack>
       </PaperProvider>
     </SafeAreaProvider>
+              </GestureHandlerRootView>
   );
 }
